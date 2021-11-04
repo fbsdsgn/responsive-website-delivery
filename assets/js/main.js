e14b50aec2a5936c0adeca45e5eb8a166c6bfb0d
Felipe Souza <fbsdsgn@gmail.com>
2021-11-04 16:07:42 -0300

    add styles and structure html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
+  // Validate that variables exist
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SHOW SCROLL UP ===============*/
-
 
 /*=============== DARK LIGHT THEME ===============*/
